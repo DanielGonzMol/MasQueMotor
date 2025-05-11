90884b04cf8457a3e6eb2a545e0246ade177bdf1
dandare <dandarexxi@gmail.com>
2025-05-11 13:35:09 +0200

    fix: fixed github deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-// src/index.js
-
 const express = require('express');
 const dotenv = require('dotenv');
-const authRoute = require('./routes/auth'); // Ruta de autenticación
+//const authRoute = require('./routes/auth'); // Ruta de autenticación
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Servir archivos estáticos de la carpeta 'public'
+app.use(express.static('public'));  // Esto servirá la carpeta 'public' directamente
+
 app.use(express.json());
-app.use('/api/auth', authRoute); // Usar la ruta de autenticación
+//app.use('/api/auth', authRoute); // Usar la ruta de autenticación
 
-
+// Ruta principal para asegurar que todo esté funcionando
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
